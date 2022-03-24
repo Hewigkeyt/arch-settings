637dc9e53ffae3906aca9114e7abc5af1e55ee4c
hewigkeyt <hewigkeyt@gmail.com>
2022-03-25 00:36:56 +0900

    replace copies by symbolic links

--- keyboard_backlight.sh
+++ keyboard_backlight.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/bash
-
-set -x
-
-file="/sys/devices/platform/sony-laptop/kbd_backlight"
-current=$(cat $file)
-if [ "$current" == "0" ]
-	then echo 1 > $file
-elif [ "$current" == "1" ]
-	then echo 0 > $file
-fi
-
+../keyboard_backlight.sh
