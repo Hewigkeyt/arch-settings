a5c52a73d5ace21ba172ee18a6ef65a38002fb57
hewigkeyt <hewigkeyt@gmail.com>
2022-03-25 00:46:01 +0900

    replace symbolic links with hard links

--- keyboard_backlight.sh
+++ keyboard_backlight.sh
@@ -1,1 +1,12 @@
-../keyboard_backlight.sh
+#!/usr/bin/bash
+
+set -x
+
+file="/sys/devices/platform/sony-laptop/kbd_backlight"
+current=$(cat $file)
+if [ "$current" == "0" ]
+	then echo 1 > $file
+elif [ "$current" == "1" ]
+	then echo 0 > $file
+fi
+
