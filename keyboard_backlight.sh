2ff78b1c94e0a3b85a7d0b5d22e5180e844f1cd9
hewigkeyt <hewigkeyt@gmail.com>
2022-03-26 19:32:21 +0900

    -1 case

--- keyboard_backlight.sh
+++ keyboard_backlight.sh
@@ -4,9 +4,9 @@ set -x
 
 file="/sys/devices/platform/sony-laptop/kbd_backlight"
 current=$(cat $file)
-if [ "$current" == "0" ]
-	then echo 1 > $file
-elif [ "$current" == "1" ]
+if [ "$current" == "1" ]
 	then echo 0 > $file
+else
+	echo 1 > $file
 fi
 
